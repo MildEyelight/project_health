67c8e0fcdf0e30d851b5636093e23e2c70ee1c5c
liyh <liyh328@mail2.sysu.edu.cn>
2022-11-17 14:31:11 +0800

    Run bash for linux

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+function randsleep(){
+    random_num=$RANDOM
+    sleep_time=`expr ${random_num} / 10` 
+    echo "Program will run in ${sleep_time} seconds" 
+    sleep ${sleep_time}s #random delay 0~3276s
+}
+# if [-e ${log_file}]
+# then
+#     continue
+# else
+    
+
+export PATH=$PATH:$pwd
+randsleep
+cd /home/liyh/SYSU-HealthReport/
+#nohup python -u /home/liyh/SYSU-HealthReport/jksb_sysu.py >> /home/liyh/SYSU-HealthReport/nohup.out
