9de0034afbeacd60e734a79db5985b5ba1270e8b
liyh <liyh328@mail2.sysu.edu.cn>
2022-11-17 17:21:03 +0800

    run file

--- run.sh
+++ run.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-function randsleep(){
-    random_num=$RANDOM
-    sleep_time=`expr ${random_num} / 10` 
-    echo "Program will run in ${sleep_time} seconds" 
-    sleep ${sleep_time}s #random delay 0~3276s
-}
-# if [-e ${log_file}]
-# then
-#     continue
-# else
-    
+now_dir=$(cd `dirname $0`; pwd)
+log_file=$now_dir/$(date +"%Y-%m-%d").log.out
 
-export PATH=$PATH:$pwd
-randsleep
-cd /home/liyh/SYSU-HealthReport/
+random_num=$RANDOM
+sleep_time=`expr ${random_num} / 10` 
+echo "Program will run in ${sleep_time} seconds" >> $log_file 
+sleep ${sleep_time}s #random delay 0~3276s
+export PATH=$PATH:$now_dir
+
+
 #填入账号密码
 netid=""
 password=""
-nohup python -u /home/liyh/SYSU-HealthReport/main.py --netid ${netid} --password ${password}>> /home/liyh/SYSU-HealthReport/nohup.out
+nohup python -u $now_dir/main.py --netid ${netid} --password ${password} >> $log_file
